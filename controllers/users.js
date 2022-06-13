6a1db8943c55029f91b1cb59276cf1d8944b82fc
pnumbers <pnumbers210@gmail.com>
2022-06-12 22:59:12 -0400

    Initial commit. Added views, controllers, routes, and app files

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const User = require('../models/user');
+
+module.exports.renderRegister = (req, res) => {
+    res.render('users/register');
+}
+
+module.exports.register = async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body
+        const user = new User({ email, username })
+        const registerUser = await User.register(user, password);
+        req.login(registerUser, (err) => {
+            if (err) return next(err);
+            // Else
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds')
+        })
+
+    } catch (e) {
+        req.flash('error', e.message)
+        res.redirect('/register')
+    }
+}
+
+module.exports.renderLoginForm = (req, res) => {
+    res.render('users/login');
+}
+
+module.exports.login = (req, res) => {
+    req.flash('success', 'Welcome back!');
+    console.log("returnTo = ", req.session.returnTo)
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            return next();
+        }
+    });
+    req.flash('success', "You've successfuly logged out.")
+    res.redirect('/campgrounds');
+}
---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelper')
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const rand1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '629d486b39b00c40db9274d7',
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit consequatur harum similique cum facere, ex, ullam quae dolor tempora et, voluptatem voluptas alias fugit! Tempora sint consequatur esse iusto eum.',
+            price
+        });
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close()
+})
