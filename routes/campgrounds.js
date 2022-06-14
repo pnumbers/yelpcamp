9419008cda9a5d60040e5ffce209f829605191b1
pnumbers <pnumbers210@gmail.com>
2022-06-13 22:44:38 -0400

    Added image file upload to show page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,13 +4,19 @@ const campgrounds = require('../controllers/campgrounds')
 const Campground = require('../models/campground');
 
 const catchAsync = require('../utils/catchAsync');
-
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.creteCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.creteCampground));
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send('IT WORKED!')
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
