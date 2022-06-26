84fc4c4fdac986d4b03b4e5525394f9506de61d0
pnumbers <pnumbers210@gmail.com>
2022-06-26 16:23:14 -0400

    Added css style sheets

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,13 +1,15 @@
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     // style: 'mapbox://styles/mapbox/dark-v10',
     // style: 'mapbox://styles/mapbox/light-v10',
     style: 'mapbox://styles/pnumbers/cl4nn51i3000s14myhe1ko6l6',
     center: [-103.5917, 40.6699],
     zoom: 3
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', () => {
     // Add a new source from our GeoJSON data and
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -7,6 +7,9 @@ const map = new mapboxgl.Map({
     zoom: 9 // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
+
 const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
     `<h3>${campground.title}</h3><p>${campground.location}</p>`
 );
